4749ba757aaa49ecccb3617235776d3f38f0d251
santiagofaginoli <102366056+santiagofaginoli@users.noreply.github.com>
2022-12-04 18:19:14 -0300

    Add files via upload

---
+++ pagina_principal/js/main.js
@@ -0,0 +1,19 @@
+const cotenedor =  document.querySelector('#contenedor');
+
+document.querySelector('#boton-menu').addEventListener('click', () => {
+    contenedor.classList.toggle('active');
+});
+
+const comprobarAncho = () => {
+    if(window.innerWidth <= 768){
+        contenedor.classList.remove('active');
+    }else{
+        contenedor.classList.add('active');
+    }
+}
+
+comprobarAncho();
+
+window.addEventListener('resize', () => {
+    comprobarAncho();
+});
